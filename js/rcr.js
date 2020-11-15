d50a1c881a75607d028786296e661dbef1e110a8
Bingqian YIN <yinbingqian1996@163.com>
2020-11-15 17:38:39 +0100

    added carrot and rabbit

--- js/rcr.js
+++ js/rcr.js
@@ -1,11 +1,30 @@
-var carrot = document.getElementById("carrot");
 
-var context = carrot.getContext('2d');
-var centerX = carrot.width / 2;
-var centerY = carrot.height / 2;
-var radius = 40;
+function drawCarrot() {
+    var carrot = document.getElementById("carrot");
+    var ctx = carrot.getContext("2d");
+    var img = document.getElementById("carrot_img");
+    ctx.drawImage(img,0,0);
+}
 
-context.beginPath();
-context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-context.fillStyle = 'orange';
-context.fill();
+function drawRiver() {
+    var river = document.getElementById("river");
+    var ctx = river.getContext("2d");
+    ctx.fillStyle = "#AFEEEE";
+    ctx.fillRect(0,0,1200,500);
+
+}
+
+function drawRabbit() {
+        var rabbit = document.getElementById("rabbit");
+        var ctx = rabbit.getContext("2d");
+        var img = document.getElementById("rabbit_img");
+        ctx.drawImage(img,0,0);
+}
+
+
+window.onload = function() {
+    drawCarrot();
+    drawRiver();
+    drawRabbit();
+};
+
